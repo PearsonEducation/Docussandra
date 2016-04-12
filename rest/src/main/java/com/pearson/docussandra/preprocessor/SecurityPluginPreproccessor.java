56144b25da897883d3a8905485f1110f6306b814
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2016-04-12 15:47:09 -0600

    javadoc and a start on sec plugins

---
+++ rest/src/main/java/com/pearson/docussandra/preprocessor/SecurityPluginPreproccessor.java
@@ -0,0 +1,14 @@
+package com.pearson.docussandra.preprocessor;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author https://github.com/JeffreyDeYoung
+ */
+public class SecurityPluginPreproccessor implements Serializable
+{
+    private Object readResolved(){
+        return null;
+    }
+}
