6c49600c6e82b3bb01ee2a683f2aeb468db717c0
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2016-04-14 11:22:07 -0600

    added more tests for the sec plugin and some cleanup

---
+++ rest/src/test/java/com/pearson/docussandra/preprocessor/SecurityPluginPreproccessorTest.java
@@ -0,0 +1,115 @@
+package com.pearson.docussandra.preprocessor;
+
+import com.pearson.docussandra.plugins.PluginHolder;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.restexpress.Request;
+import org.restexpress.Response;
+import org.restexpress.route.RouteResolver;
+
+/**
+ *
+ * @author https://github.com/JeffreyDeYoung
+ */
+public class SecurityPluginPreproccessorTest
+{
+
+    /**
+     * Plugin holder for this test. Global and static for better reuse.
+     */
+    private static PluginHolder ph;
+
+    public SecurityPluginPreproccessorTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+        File pluginJar = new File("./src/test/resources", "plugin-test-notify.jar");
+        File[] jars = new File[1];
+        jars[0] = pluginJar;
+        try
+        {
+            ph = PluginHolder.build(jars);
+        } catch (IllegalStateException e)
+        {
+            ph = PluginHolder.getInstance();
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of process method, of class SecurityPluginPreproccessor.
+     */
+    @Test
+    public void testProcess()
+    {
+        System.out.println("process");
+        Request request = createTestRequest();
+        Response response = null;
+        SecurityPluginPreproccessor instance = new SecurityPluginPreproccessor();
+        instance.process(request, response);
+
+    }
+
+    /**
+     * Test of extractHeadersFromRequest method, of class
+     * SecurityPluginPreproccessor.
+     */
+    @Test
+    public void testExtractHeadersFromRequest()
+    {
+        System.out.println("extractHeadersFromRequest");
+        Request request = createTestRequest();
+        HashMap<String, List<String>> result = SecurityPluginPreproccessor.extractHeadersFromRequest(request);
+        assertNotNull(result);
+        assertTrue(result.containsKey("testHeader_1"));
+        assertTrue(result.containsKey("testHeader_2"));
+        assertNotNull(result.get("testHeader_1"));
+        assertNotNull(result.get("testHeader_2"));
+        assertNotNull(result.get("testHeader_1").get(0));
+        assertNotNull(result.get("testHeader_1").get(1));
+        assertNotNull(result.get("testHeader_1").get(2));
+        assertNotNull(result.get("testHeader_2").get(0));
+        assertEquals("test1", result.get("testHeader_1").get(0));
+        assertEquals("test2", result.get("testHeader_1").get(1));
+        assertEquals("test3", result.get("testHeader_1").get(2));
+    }
+
+    private Request createTestRequest()
+    {
+        Request r = new Request(new DefaultFullHttpRequest(HttpVersion.HTTP_1_0, HttpMethod.GET, "/test/path/here"), new RouteResolver(null));
+        r.addHeader("testHeader_1", "test1");
+        r.addHeader("testHeader_1", "test2");
+        r.addHeader("testHeader_1", "test3");
+        r.addHeader("testHeader_2", "test4");
+        return r;
+    }
+
+}
