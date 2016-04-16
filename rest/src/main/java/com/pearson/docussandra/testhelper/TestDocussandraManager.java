019a7486f5b0451d9e5e16b22d13dfd1f8a93b1a
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2016-04-16 17:43:51 -0600

    better test structure; prep for using testing framework

---
+++ rest/src/main/java/com/pearson/docussandra/testhelper/TestDocussandraManager.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2015/2016 https://github.com/JeffreyDeYoung.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pearson.docussandra.testhelper;
+
+import com.pearson.docussandra.Main;
+import java.io.IOException;
+import org.restexpress.RestExpress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class for managing testing of startup of Docussandra so we are not tearing
+ * down and creating new RestExpress server instances with every test.
+ *
+ * For test use only!
+ *
+ * The alternate strange reason for creating this is that it seems once our
+ * RestExpress server is started up, it won't let go of the HyperExpress plugin
+ * and will disallow another RestExpress instance to startup, even after the
+ * first has been shutdown.
+ *
+ * Generally, the usage is as follows:
+ * TestDocussandraManger.getManager().ensureTestDocussandraRunning();
+ *
+ * @author https://github.com/JeffreyDeYoung
+ */
+public class TestDocussandraManager
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestDocussandraManager.class);
+
+    private static TestDocussandraManager manager = null;
+
+    private static boolean restExpressRunning = false;
+
+    private static RestExpress server;
+
+    /**
+     * Singleton.
+     */
+    private TestDocussandraManager()
+    {
+        ;
+    }
+
+    /**
+     * Gets an instance of this class.
+     *
+     * @return
+     */
+    public static synchronized TestDocussandraManager getManager()
+    {
+        if (manager == null)
+        {
+            manager = new TestDocussandraManager();
+        }
+        return manager;
+    }
+
+    /**
+     * Ensures RestExpress is presently running. The rest endpoints will be
+     * exposed on port 19080.
+     *
+     * Cassandra will be mocked internally instead of relying on a external
+     * process; no data will be saved past the JVM shutdown.
+     *
+     * @param keyspace Keyspace to use.
+     * @throws Exception
+     */
+    public synchronized void ensureTestDocussandraRunningWithMockCassandra(String keyspace) throws Exception
+    {
+        Fixtures.ensureMockCassandraRunningAndEstablished(keyspace);
+        ensureTestDocussandraRunning(true);
+    }
+
+    /**
+     * Ensures RestExpress is presently running. The rest endpoints will be
+     * exposed on port 19080.
+     *
+     * Cassandra will be mocked internally instead of relying on a external
+     * process; no data will be saved past the JVM shutdown.
+     *
+     * @throws IOException
+     */
+    public synchronized void ensureTestDocussandraRunning() throws IOException, IllegalAccessException, InstantiationException
+    {
+        ensureTestDocussandraRunning(true);//default to mocking cassandra
+    }
+
+    /**
+     * Ensures RestExpress is presently running. The rest endpoints will be
+     * exposed on port 19080.
+     *
+     * @param mockCassandra If true, Cassandra will be mocked instead of relying
+     * on a external process. If false, this test instance of Docussandra will
+     * hit an internal mocked version of Cassandra instead; no data will be
+     * saved past the JVM shutdown.
+     *
+     * @throws IOException
+     */
+    public synchronized void ensureTestDocussandraRunning(boolean mockCassandra) throws IOException, IllegalAccessException, InstantiationException
+    {
+        if (restExpressRunning == false)
+        {
+            LOGGER.info("Starting RestExpress server...");
+            if (mockCassandra)
+            {
+                String[] params = new String[1];
+                params[0] = "local_test";
+                server = Main.initializeServer(params, null);
+            } else
+            {
+                server = Main.initializeServer(new String[0], null);
+            }
+            restExpressRunning = true;
+        }
+    }
+
+    /**
+     * Ensures RestExpress is presently running. The rest endpoints will be
+     * exposed on port 19080.
+     *
+     * @param cassandraSeeds Cassandra seeds to use when starting up
+     * RestExpress. Will override any existing seeds.
+     *
+     * @throws IOException
+     */
+    public synchronized void ensureTestDocussandraRunning(String cassandraSeeds) throws IOException, IllegalAccessException, InstantiationException
+    {
+        if (restExpressRunning == false)
+        {
+            LOGGER.info("Starting RestExpress server...");
+            if (cassandraSeeds != null)
+            {
+                String[] params = new String[1];
+                params[0] = "local_test";
+                server = Main.initializeServer(params, cassandraSeeds);
+            } else
+            {
+                server = Main.initializeServer(new String[0], null);
+            }
+            restExpressRunning = true;
+        }
+    }
+
+    /**
+     * Shuts down the rest express instance.
+     *
+     * @throws Throwable
+     */
+    @Override
+    public void finalize() throws Throwable
+    {
+        super.finalize();
+        if (server != null)
+        {
+            server.shutdown(true);
+        }
+        restExpressRunning = false;
+    }
+
+}
