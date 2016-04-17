d9abf76d51b9c1db2c89ed41bc0e06cc2846bb10
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2016-04-16 21:19:27 -0600

    added test for test helper

---
+++ rest/src/test/java/com/pearson/docussandra/testhelper/TestDocussandraManagerTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pearson.docussandra.testhelper;
+
+import com.jayway.restassured.RestAssured;
+import static com.jayway.restassured.RestAssured.expect;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jeffrey DeYoung
+ */
+public class TestDocussandraManagerTest
+{
+
+    public TestDocussandraManagerTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of getManager method, of class TestDocussandraManager.
+     */
+    @Test
+    public void testGetManager()
+    {
+        System.out.println("getManager");
+        TestDocussandraManager result = TestDocussandraManager.getManager();
+        assertNotNull(result);
+        TestDocussandraManager result2 = TestDocussandraManager.getManager();
+        assertNotNull(result2);
+        assertEquals(result, result2);
+    }
+
+    /**
+     * Test of ensureTestDocussandraRunningWithMockCassandra method, of class
+     * TestDocussandraManager.
+     */
+    @Test
+    public void testEnsureTestDocussandraRunningWithMockCassandra() throws Exception
+    {
+        System.out.println("ensureTestDocussandraRunningWithMockCassandra");
+        String keyspace = "docussandra";
+        TestDocussandraManager instance = TestDocussandraManager.getManager();
+        instance.ensureTestDocussandraRunningWithMockCassandra(keyspace);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 19080;
+        RestAssured.basePath = "/";
+        expect().statusCode(200)
+                .body("isHealthy", equalTo(true))
+                .get("/admin/health");
+    }
+
+}
